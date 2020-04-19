3a9e4e9be2b7ae286bd017f7a38f26ee4215b7ad
Marcelo Correa <marcelocorrea.0827@outlook.com>
2020-04-19 14:08:31 -0300

    Criacao da Solucao com Try/Catch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using ExercPropTratExcecoes.Entities;
+using ExercPropTratExcecoes.Exceptions;
 
 namespace ExercPropTratExcecoes
 {
@@ -6,6 +9,37 @@ namespace ExercPropTratExcecoes
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                // Instanciando Account
+                Account conta = new Account(number, holder, balance, withdrawLimit);
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta.Withdraw(amount);
+                Console.WriteLine(conta);
+            }
+            catch (DomainException e) {
+                Console.WriteLine("Error in Account: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
         }
     }
 }
